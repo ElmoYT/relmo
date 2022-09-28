35b5ffcc0ab53bb3cdb31241fd6bd39a0de5a1b1
ElmoYT <92131494+ElmoYT@users.noreply.github.com>
2022-09-28 13:31:18 +1000

    Add files via upload

---
+++ relmo-run.bash
@@ -0,0 +1,10 @@
+echo "BUILDING"
+echo "[5s]"
+echo "[4s]"
+echo "[3s]"
+echo "[2s]"
+echo "[1s]"
+echo "[0s]"
+echo "BUILD COMPLETE"
+echo "********************************"
+java src/com/java/Main.java
